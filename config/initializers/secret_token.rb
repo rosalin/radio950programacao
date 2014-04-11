345798509266f7c7e26784970b4a84d44ebc18a2
Eduardo <eduardo@eduardo-Aspire-E1-471.(none)>
2014-04-11 14:22:35 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radio950::Application.config.secret_token = 'b431febe09c4c541550ec012972c8c78539884dfe39226df6fd94993e9e71051f9618d5146dbb3f3170c99a4cc1e1de6d3a991efa548173baaf8ee50e1cd4bf9'
